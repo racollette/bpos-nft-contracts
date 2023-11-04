3049194dc23dd56adf9953cee69c7d920d5a7d1f
Ryan <racollette@gmail.com>
2023-11-03 20:15:05 -0400

    edit contracts and add metadata

---
+++ contracts/NFT3.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract Proof_of_Integrity is ERC721URIStorage, Ownable {
+    uint256 public constant maxMintableNFTs = 88; // Define the maximum NFTs that can be minted.
+    uint256 public totalMintedNFTs = 0; // Keep track of the total minted NFTs.
+
+    constructor(
+        address initialOwner
+    ) Ownable(initialOwner) ERC721("Proof of Integrity", "PoI") {}
+
+    function mintNFT(
+        address recipient,
+        string memory tokenURI
+    ) public payable virtual returns (uint256) {
+        require(totalMintedNFTs < maxMintableNFTs, "Max NFT limit reached");
+        require(msg.value >= 20000000000000000, "Not enough ELA sent!");
+
+        totalMintedNFTs++;
+
+        _mint(recipient, totalMintedNFTs);
+        _setTokenURI(totalMintedNFTs, tokenURI);
+
+        return totalMintedNFTs;
+    }
+
+    // Function to allow the owner to change the tokenURI for a specific NFT.
+    function setTokenURI(
+        uint256 tokenId,
+        string memory newTokenURI
+    ) public onlyOwner {
+        require(tokenId <= totalMintedNFTs, "Token ID does not exist");
+        _setTokenURI(tokenId, newTokenURI);
+    }
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,14 +6,22 @@ async function main() {
   console.log(process.env.API_URL);
   console.log(process.env.PRIVATE_KEY);
   // Grab the contract factory
-  const MyNFT = await ethers.getContractFactory("MyNFT");
   // const signer = (await ethers.getSigners())[0]; // You can choose the signer you want to use
 
   // console.log(MyNFT);
 
-  const myNFT = await MyNFT.deploy({ gasPrice: 10000000000, gasLimit: 3000000 });
+  const nft1 = await ethers.getContractFactory("Proof_of_Work");
+  const NFT1 = await nft1.deploy("0x3Ac3935326cCA3146365a39458b5905bCdAe476b", { gasPrice: 10000000000, gasLimit: 3000000 });
+  console.log("PoW Contract deployed to address:", NFT1.target);
 
-  console.log("Contract deployed to address:", myNFT.target);
+  const nft2 = await ethers.getContractFactory("Bonded_Proof_of_Stake");
+  const NFT2 = await nft2.deploy("0x3Ac3935326cCA3146365a39458b5905bCdAe476b", { gasPrice: 10000000000, gasLimit: 3000000 });
+  console.log("BPoS Contract deployed to address:", NFT2.target);
+
+  const nft3 = await ethers.getContractFactory("Proof_of_Integrity");
+  const NFT3 = await nft3.deploy("0x3Ac3935326cCA3146365a39458b5905bCdAe476b", { gasPrice: 10000000000, gasLimit: 3000000 });
+  console.log("PoI Contract deployed to address:", NFT3.target);
+
 }
 
 main()
